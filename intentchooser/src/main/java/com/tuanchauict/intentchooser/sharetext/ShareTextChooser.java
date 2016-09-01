cd423d58b3ee3a4dcc88362abc4e30918cdb2e46
Tuan Chau <tuanchauict@gmail.com>
2016-09-01 12:23:57 +0700

    Minor change

--- intentchooser/src/main/java/com/tuanchauict/intentchooser/sharetext/ShareTextChooser.java
+++ intentchooser/src/main/java/com/tuanchauict/intentchooser/sharetext/ShareTextChooser.java
@@ -1,14 +1,6 @@
 package com.tuanchauict.intentchooser.sharetext;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.util.Pair;
-
 import com.tuanchauict.intentchooser.ChooserMaker;
-
-import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by tuanchauict on 8/30/16.
